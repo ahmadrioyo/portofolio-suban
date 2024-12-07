5cdca4d43299c8c1c1b14aeae6b907efb96c3223
ahmadrioyo <118823654+ahmadrioyo@users.noreply.github.com>
2024-12-07 22:47:09 +0700

    layout + progres

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full bg-blue-400">
+<html lang="en" class="h-full bg-sky-300">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,48 +1,54 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <div class="overflow-hidden rounded bg-white">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div class="lg:pr-8 lg:pt-4">
-              <div class="lg:max-w-lg">
-                <h2 class="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-                <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">A better workflow</p>
-                <p class="mt-6 text-lg/8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
-                <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-                  <div class="relative pl-9">
-                    <dt class="inline font-semibold text-gray-900">
-                      <svg class="absolute left-1 top-1 size-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                        <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 0 1-1.44-8.765 4.5 4.5 0 0 1 8.302-3.046 3.5 3.5 0 0 1 4.504 4.272A4 4 0 0 1 15 17H5.5Zm3.75-2.75a.75.75 0 0 0 1.5 0V9.66l1.95 2.1a.75.75 0 1 0 1.1-1.02l-3.25-3.5a.75.75 0 0 0-1.1 0l-3.25 3.5a.75.75 0 1 0 1.1 1.02l1.95-2.1v4.59Z" clip-rule="evenodd" />
-                      </svg>
-                      Push to deploy.
-                    </dt>
-                    <dd class="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+    <div class="relative overflow-hidden bg-white rounded-lg">
+        <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+          <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+            <div class="sm:max-w-lg">
+              <h3 class="text-2xl tracking-tight text-gray-900 sm:text-4xl">Hello Everyone, I am</h3>
+              <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Ahmad Maulana Subandrio.</h1>
+              <p class="mt-2 text-xl text-gray-500">welcome to my personal website portofolio.</p>
+            </div>
+            <div>
+              <div class="mt-10">
+                <!-- Decorative image grid -->
+                <div aria-hidden="true" class="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
+                  <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                    <div class="flex items-center space-x-6 lg:space-x-8">
+                      <div class="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                        <div class="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                        <div class="h-64 w-44 overflow-hidden rounded-lg">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                      </div>
+                      <div class="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                        <div class="h-64 w-44 overflow-hidden rounded-lg">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                        <div class="h-64 w-44 overflow-hidden rounded-lg">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                        <div class="h-64 w-44 overflow-hidden rounded-lg">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                      </div>
+                      <div class="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                        <div class="h-64 w-44 overflow-hidden rounded-lg">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                        <div class="h-64 w-44 overflow-hidden rounded-lg">
+                          <img src="{{ asset('assets/profile.jpg') }}" alt="" class="size-full object-cover">
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div class="relative pl-9">
-                    <dt class="inline font-semibold text-gray-900">
-                      <svg class="absolute left-1 top-1 size-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                        <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
-                      </svg>
-                      SSL certificates.
-                    </dt>
-                    <dd class="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</dd>
-                  </div>
-                  <div class="relative pl-9">
-                    <dt class="inline font-semibold text-gray-900">
-                      <svg class="absolute left-1 top-1 size-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                        <path d="M4.632 3.533A2 2 0 0 1 6.577 2h6.846a2 2 0 0 1 1.945 1.533l1.976 8.234A3.489 3.489 0 0 0 16 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234Z" />
-                        <path fill-rule="evenodd" d="M4 13a2 2 0 1 0 0 4h12a2 2 0 1 0 0-4H4Zm11.24 2a.75.75 0 0 1 .75-.75H16a.75.75 0 0 1 .75.75v.01a.75.75 0 0 1-.75.75h-.01a.75.75 0 0 1-.75-.75V15Zm-2.25-.75a.75.75 0 0 0-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75h-.01Z" clip-rule="evenodd" />
-                      </svg>
-                      Database backups.
-                    </dt>
-                    <dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</dd>
-                  </div>
-                </dl>
+                </div>
+      
+                <a href="#" class="inline-block rounded-md border border-transparent bg-sky-600 px-8 py-3 text-center font-medium text-white hover:bg-sky-700">Let's go &raquo;</a>
               </div>
             </div>
-            <img src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
           </div>
         </div>
-      </div>
-      
+    </div>      
 </x-layout>
