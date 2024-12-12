b385570cfe3386808c74d1fc821d7f56c831ad3f
ahmadrioyo <118823654+ahmadrioyo@users.noreply.github.com>
2024-12-13 00:50:23 +0700

    progres navbar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,8 +11,49 @@
     <link rel="icon" type="image/png" href="{{ asset('assets/logo.png') }}">
 </head>
 <body>
+    {{-- header start --}}
+    <header class="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+        <div class="container">
+            <div class="flex items-center justify-between relative">
+                <div class="px-4">
+                    <a href="#home" class="font-bold text-lg text-cyan-500 block py-6">Suban</a>
+                </div>
+                <div class="flex items-center px-4">
+                    <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden">
+                        <span class="hamburger-line transition duration-400 ease-in-out origin-top-left"></span>
+                        <span class="hamburger-line transition duration-400 ease-in-out"></span>
+                        <span class="hamburger-line transition duration-400 ease-in-out origin-bottom-left"></span>
+                    </button>
+                    <nav id="nav-menu" class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+                        <ul class="block lg:flex">
+                            <li class="group">
+                                <a href="#home" class="text-base text-slate-950 py-2 mx-8 flex group-hover:text-cyan-500">Beranda</a>
+                            </li>
+                            <li class="group">
+                                <a href="#about" class="text-base text-slate-950 py-2 mx-8 flex group-hover:text-cyan-500">About Me</a>
+                            </li>
+                            <li class="group">
+                                <a href="#project" class="text-base text-slate-950 py-2 mx-8 flex group-hover:text-cyan-500">My Projects</a>
+                            </li>
+                            <li class="group">
+                                <a href="#client" class="text-base text-slate-950 py-2 mx-8 flex group-hover:text-cyan-500">Clients</a>
+                            </li>
+                            <li class="group">
+                                <a href="#blog" class="text-base text-slate-950 py-2 mx-8 flex group-hover:text-cyan-500">Blog</a>
+                            </li>
+                            <li class="group">
+                                <a href="#contact" class="text-base text-slate-950 py-2 mx-8 flex group-hover:text-cyan-500">Contacts</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    {{-- header end --}}
+
     {{-- hero section start --}}
-    <section id="home" class="pt-36">
+    <section id="home" class="pt-28">
         <div class="container">
             <div class="flex flex-wrap">
                 <div class="w-full self-center px-4 lg:w-1/2">
@@ -92,5 +133,7 @@
         </div>
     </section>
     {{-- about section end --}}
+
+    <script src="{{ asset('js/scripts.js') }}"></script>
 </body>
 </html>
