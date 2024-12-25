04208e7a34a6f04bdfb57c75bd93e3de750ad039
ahmadrioyo <118823654+ahmadrioyo@users.noreply.github.com>
2024-12-25 18:44:17 +0700

    add dark toggle + progress dark mode

--- src/script.js
+++ src/script.js
@@ -23,3 +23,22 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+window.addEventListener('click', function(e){
+    if( e.target !== hamburger && e.target !== navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    };
+});
+
+// dark toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+    }else{
+        html.classList.remove('dark');
+    }
+});
